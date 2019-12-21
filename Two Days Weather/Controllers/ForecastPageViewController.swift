f5fae246daf23ac992548bc7b63bfdfcefec53f6
Kyle Coburn <kylecoburn@gmail.com>
2019-12-21 14:08:42 -0800

    [feat] add page control indicator

--- Two Days Weather/Controllers/ForecastPageViewController.swift
+++ Two Days Weather/Controllers/ForecastPageViewController.swift
@@ -4,10 +4,23 @@ final class ForecastPageViewController: UIPageViewController {
 
 	private let forecastViewControllers = [ForecastViewController(), ForecastViewController()]
 
+	private let pageControl = UIPageControl()
+
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		dataSource = self
+		delegate = self
 		setViewControllers([forecastViewControllers[0]], direction: .forward, animated: false)
+
+		pageControl.currentPageIndicatorTintColor = .white
+		pageControl.pageIndicatorTintColor = .lightGray
+		pageControl.numberOfPages = forecastViewControllers.count
+		pageControl.currentPage = 0
+		view.addSubview(pageControl)
+
+		pageControl.translatesAutoresizingMaskIntoConstraints = false
+		pageControl.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+		pageControl.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: 0).isActive = true
 	}
 
 	func update(forecasts: TodayAndTomorrowsForecast) {
@@ -17,15 +30,31 @@ final class ForecastPageViewController: UIPageViewController {
 
 }
 
+// MARK: - UIPageViewControllerDelegate
+
+extension ForecastPageViewController: UIPageViewControllerDelegate {
+
+	func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
+		if let viewController = viewControllers?.first, let index = getIndex(of: viewController) {
+			pageControl.currentPage = index
+		}
+	}
+
+}
+
 // MARK: - UIPageViewControllerDataSource
 
 extension ForecastPageViewController: UIPageViewControllerDataSource {
 
-	private func getNextViewController(for viewController: UIViewController) -> UIViewController? {
+	private func getIndex(of viewController: UIViewController) -> Int? {
 		guard let viewController = viewController as? ForecastViewController else {
 			return nil
 		}
-		guard let index = forecastViewControllers.firstIndex(of: viewController) else {
+		return forecastViewControllers.firstIndex(of: viewController)
+	}
+
+	private func getNextViewController(for viewController: UIViewController) -> UIViewController? {
+		guard let index = getIndex(of: viewController) else {
 			return nil
 		}
 		return forecastViewControllers[index == 0 ? 1 : 0]
