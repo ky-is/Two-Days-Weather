907559ac8c7e555806add56ddcb1ddde859303a3
Kyle Coburn <kylecoburn@gmail.com>
2019-12-21 17:40:37 -0800

    [chore] cleanup

--- Podfile
+++ Podfile
@@ -1,6 +1,6 @@
 platform :ios, '13.0'
 
 target 'Two Days Weather' do
-  use_frameworks!
-  pod 'CouchbaseLite-Swift'
+	use_frameworks!
+	pod 'CouchbaseLite-Swift'
 end
--- Two Days Weather/AppDelegate.swift
+++ Two Days Weather/AppDelegate.swift
@@ -7,8 +7,6 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
 		return true
 	}
 
-	// MARK: UISceneSession Lifecycle
-
 	func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
 		return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
 	}
